b708ef03c3d372da3dd235443a89921b417449d1
monjuaflame <monjuaflame@gmail.com>
2023-11-10 01:23:51 +0600

    admin travel endpoint and auth sactum with login

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\V1\TourController;
-use App\Http\Controllers\Api\V1\ToursController;
 use App\Http\Controllers\Api\V1\TravelController;
+use \App\Http\Controllers\Api\V1\Admin;
+use App\Http\Controllers\Api\V1\Auth\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +24,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('travels', [TravelController::class, 'index']);
 Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
+
+Route::prefix('admin')->middleware(['auth:sanctum', 'role:admin'])->group(function(){
+    Route::post('travels', [Admin\TravelController::class, 'store']);
+});
+
+Route::post('login', LoginController::class);
---
+++ tests/Feature/AdminTravelTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Role;
+use App\Models\User;
+use Database\Seeders\RoleSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AdminTravelTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function test_public_user_cannot_access_adding_travel(): void
+    {
+        $response = $this->postJson('/api/v1/admin/travels');
+        $response->assertStatus(401);
+        
+    }
+    public function test_non_admin_cannot_access_adding_travel(): void
+    {
+        $this->seed(RoleSeeder::class);
+        $user = User::factory()->create();
+        $user->roles()->attach(Role::where('name', 'editor')->value('id'));
+        $response = $this->actingAs($user)->postJson('/api/v1/admin/travels');
+        
+        $response->assertStatus(403);
+        
+    }
+    public function test_saves_travel_successfully_with_valid_data(): void
+    {
+        $this->seed(RoleSeeder::class);
+        $user = User::factory()->create();
+        $user->roles()->attach(Role::where('name', 'admin')->value('id'));
+        $response = $this->actingAs($user)->postJson('/api/v1/admin/travels',[
+            'name' => 'Travel Name',
+        ]);
+        
+        $response->assertStatus(422);
+        $response = $this->actingAs($user)->postJson('/api/v1/admin/travels', [
+            'name' => 'Travel Name',
+            'is_public' => '1',
+            'description' => 'Some Descritption',
+            'number_of_days' => 5,
+        ]);
+
+        $response->assertStatus(201);
+
+        $response = $this->get('/api/v1/travels');
+        $response->assertJsonFragment(['name' => 'Travel Name']);
+        
+    }
+}
